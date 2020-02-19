6def00e26c1956a9acf7414ae3da8d068004f6af
jake <jake.haller.roby@gmail.com>
2020-02-19 00:49:48 -0800

    p34

---
+++ src/com/project/problems/p34.java
@@ -0,0 +1,39 @@
+package com.project.problems;
+
+import com.project.util.CharArray;
+import com.project.util.ProblemBase;
+
+import java.util.HashMap;
+
+public class p34 extends ProblemBase {
+
+    static HashMap<Integer, Integer> map = new HashMap<>();
+
+    public Long execute() {
+        init();
+        long result = 0L;
+        for(int i = 10; i <= 2540160; i++) {
+            int sum = 0;
+            int start = i;
+            while(start > 0) {
+                sum += map.get(start%10);
+                start /= 10;
+            }
+
+            if(sum == i) {
+                result += i;
+            }
+
+        }
+        return result;
+    }
+
+    public void init() {
+        int factorial = 1;
+        map.put(0,factorial);
+        for(int i = 1; i < 10; i++) {
+            factorial *= i;
+            map.put(i, factorial);
+        }
+    }
+}
